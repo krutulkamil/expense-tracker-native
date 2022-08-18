b289a1e6c5a168aaeb59d46fe691b205bff3abf8
krutulkamil <krutulkamil@onet.pl>
2022-08-18 20:08:31 +0200

    delete expense button

--- screens/ManageExpense.tsx
+++ screens/ManageExpense.tsx
@@ -1,7 +1,9 @@
 import { FunctionComponent, useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { NavigationProp, ParamListBase, RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "../types";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
 interface ManageExpenseProps {
     route: RouteProp<RootStackParamList, 'ManageExpense'>;
@@ -12,6 +14,8 @@ const ManageExpense: FunctionComponent<ManageExpenseProps> = ({ route, navigatio
     const editedExpenseId = route.params?.expenseId;
     const isEditing = !!editedExpenseId;
 
+    const deleteExpenseHandler = () => {};
+
     useLayoutEffect(() => {
         navigation.setOptions({
             title: isEditing ? "Edit Expense" : "Add Expense"
@@ -19,8 +23,34 @@ const ManageExpense: FunctionComponent<ManageExpenseProps> = ({ route, navigatio
     }, [isEditing, navigation]);
 
     return (
-        <Text>Manage Expense Screen</Text>
+        <View style={styles.container}>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton
+                        icon="trash"
+                        color={GlobalStyles.colors.error500}
+                        size={36}
+                        onPress={deleteExpenseHandler}
+                    />
+                </View>
+            )}
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: "center"
+    }
+});
 
 export default ManageExpense;
